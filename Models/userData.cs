8e76c11f7d15aa362fedf99cdfae85ef9232363e
Zane Wilson <zw540816@ohio.edu>
2020-10-14 21:04:08 -0400

    Added New Model for Profile Creation - Zane

---
+++ Models/userData.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CentricTeam4.Models
+{
+    public class userData
+    {
+        [Required] 
+        public Guid ID { get; set; } 
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter your most frequently used email address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+
+        [Required] 
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Primary Phone")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-\d{4}$",
+               ErrorMessage ="Phone Numbers must be in the format (xxx) xxx-xxxx or xxx-xxx-xxxx")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Current Position")]
+        public string Position { get; set; }
+
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
+
+        [Display(Name = "Bio")]
+        public string bio { get; set; }
+
+       
+
+
+    }
+}
